079d54258a1baee9eeaf5b1af5f5cc9106cd2f4b
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-02-24 07:14:48 +0200

    adding dev server, working 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = (props) =>  {
   return (
@@ -13,14 +14,19 @@ const Persons = (props) =>  {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { 
-       name: 'Arto Hellas',
-       number: '040-1234567'
-    }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fullfilled: ', response)
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleChangeName = (event) => {
     setNewName(event.target.value)
