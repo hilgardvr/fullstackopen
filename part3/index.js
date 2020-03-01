fd4c8f976ce8b8df4090a54f1c16119f2e9ff0a6
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-03-01 07:57:47 +0200

    3.4

--- part3/index.js
+++ part3/index.js
@@ -29,6 +29,28 @@ app.get('/', (req, res) => {
     res.send('<h1>Root of API</h>')
 })
 
+app.get('/info', (req, res) => {
+    const html = `  <p>Phonebook has info for ${persons.length} people</p>
+                    <p>${new Date()}</p>`
+    res.send(html)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.send(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const pid = Number(req.params.id)
+    persons = persons.filter(({id}) => pid !== id)
+    res.status(204).end()
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
